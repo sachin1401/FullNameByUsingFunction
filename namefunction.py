655304198d478f90ff38b1e12fff123928a7c9cb
Sachin Patel <85048630+sachin1401@users.noreply.github.com>
2022-04-04 09:07:11 +0530

    Add files via upload

---
+++ namefunction.py
@@ -0,0 +1,7 @@
+def print_full_name(first_name, last_name):
+    print(f"Hello {first_name} {last_name}! You done amazing job")
+    
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
